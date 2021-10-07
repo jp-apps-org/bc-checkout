cc28ef76e9cc9882174404b539a050207e1bbe87
Pushkar Goel <pushkar@Pushkars-MacBook-Pro.local>
2021-10-07 18:10:06 +0530

    Changed order-confirmation screen url

--- src/app/checkout/navigateToOrderConfirmation.tsx
+++ src/app/checkout/navigateToOrderConfirmation.tsx
@@ -1,9 +1,9 @@
 import { noop } from 'lodash';
 
 export default function navigateToOrderConfirmation(location = window.location): Promise<never> {
-    const url = `${location.pathname.replace(/\/$/, '')}/order-confirmation`;
+    // const url = `${location.pathname.replace(/\/$/, '')}/order-confirmation`;
+    // location.replace(url);
 
-    location.replace(url);
-
+    location.replace("http://100.26.223.23:3000/order-confirmation");
     return new Promise(noop);
 }
