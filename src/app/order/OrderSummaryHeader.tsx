73a859e327c1cc4a7ca4160d66098f19b5300bb4
Pushkar Goel <pushkar@192.168.1.6>
2021-12-18 01:05:20 +0530

    Added production build

--- src/app/order/OrderSummaryHeader.tsx
+++ src/app/order/OrderSummaryHeader.tsx
@@ -3,15 +3,18 @@ import React, { FunctionComponent } from 'react';
 import { TranslatedString } from '../locale';
 
 const OrderSummaryHeader: FunctionComponent = ({ children }) => (
-    <header className="cart-header">
-        <h3 className="cart-title optimizedCheckout-headingSecondary">
-            <TranslatedString id="cart.cart_heading" />
-        </h3>
-        <div>
+    <div>
+        <header className="cart-header">
+            <h3 className="cart-title optimizedCheckout-headingSecondary">
+                <TranslatedString id="cart.cart_heading" />
+            </h3>
+        { children }
+        </header>
+        <div style={{ padding: "0.5rem 1.5rem" }}>
             <TranslatedString id="cart.cart_sub_heading" />
         </div>
-       { children }
-    </header>
+    </div>
+    
 );
 
 export default OrderSummaryHeader;
