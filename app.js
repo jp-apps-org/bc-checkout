dc5efd97ec5561140b857a9ee7470b724412699f
Pushkar Goel <pgpushkar@gmail.com>
2024-03-17 18:12:21 +0530

    Added express as static server

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = express();
+app.use(express.static("dist"));
+
+const port = process.env.PORT || 3000;
+app.listen(port);
+console.log(`--------- server started on port ${port} ----------`);
